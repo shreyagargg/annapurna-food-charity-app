f59bc576c9302075731f23ae0fe89d396938e9b8
shreya <shreyagarg06031412@gmail.com>
2024-09-16 19:36:44 +0530

    logo, login screen, registration or donation(except image)

---
+++ app/src/main/java/com/example/annpurna/DonorModel.kt
@@ -0,0 +1,14 @@
+package com.example.annpurna
+
+data class DonorModel(
+    val foodName : String? = null,
+    val foodDesc : String? = null,
+    val expiryDate : String? = null,
+    val foodQuantity : String? = null,
+    val foodImage : String? = null,
+    val address : String? = null,
+    val state : String? = null,
+    val city : String? = null,
+    val pinCode : Int? = null
+)
+
